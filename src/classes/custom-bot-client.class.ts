1f18a0aa604f2c91b84d0aaaf31e10f3add79824
Marcelo Soares <33166091+marcelotsoares@users.noreply.github.com>
2022-12-19 15:59:54 -0300

    refactor: unused functions removed

--- src/classes/custom-bot-client.class.ts
+++ src/classes/custom-bot-client.class.ts
@@ -1,12 +1,5 @@
-import { Client, MessageEmbed, Message } from 'discord.js';
+import { MessageEmbed } from 'discord.js';
 import { CustomBot, ICustomBotOpts } from './custom-bot';
-
-interface ICreateChannel {
-    client: Client;
-    message: Message;
-    parentId: string;
-    channelId: string;
-}
 
 interface IGuildObject {
     name: string;
@@ -23,85 +16,6 @@ interface ICreateEmbed {
 export class CustomBotClient extends CustomBot {
     constructor(options: ICustomBotOpts) {
         super(options);
-    }
-
-    async createChannel(props: ICreateChannel) {
-        try {
-            const { client, channelId, message, parentId } = props;
-            if (!message) {
-                return console.log('[createChannel] Message not found');
-            }
-
-            if (!message.guild) {
-                return console.log('[createChannel] MessageGuild not found');
-            }
-
-            if (!client.user) {
-                return console.log('[createChannel] ClientUser not found');
-            }
-
-            return await message.guild.channels.create(`${channelId}-${message.author.id}`, {
-                parent: parentId,
-                permissionOverwrites: [
-                    {
-                        id: message.guild.roles.everyone,
-                        deny: 'VIEW_CHANNEL',
-                    },
-                    {
-                        id: client.user,
-                        allow: 'VIEW_CHANNEL',
-                    },
-                    {
-                        id: message.author,
-                        allow: ['SEND_MESSAGES', 'VIEW_CHANNEL'],
-                    },
-                ],
-            });
-        } catch (err) {
-            console.log('[createChannel:error]', err);
-        }
-    }
-
-    async addRoleToUser(message: Message, roleName: string) {
-        if (!message) {
-            return console.log('[addRoleToUser] Message not found');
-        }
-
-        if (!message.guild) {
-            return console.log('[addRoleToUser] MessageGuild not found');
-        }
-
-        if (!message.member) {
-            return console.log('[addRoleToUser] MessageMember not found');
-        }
-
-        console.log(`[addRoleToUser] roleName: ${roleName}`);
-        const role = message.guild.roles.cache.find((role) => role.name === roleName);
-        if (role) {
-            message.member.roles.add(role);
-        }
-    }
-
-    async userHasRole(message: Message, roleName: string) {
-        if (!message) {
-            return console.log('[userHasRole] Message not found');
-        }
-
-        if (!message.guild) {
-            return console.log('[userHasRole] MessageGuild not found');
-        }
-
-        if (!message.member) {
-            return console.log('[userHasRole] MessageMember not found');
-        }
-
-        const role = message.guild.roles.cache.find((role) => role.name === roleName);
-        if (role) {
-            if (message.member.roles.cache.has(role.id)) {
-                return true;
-            }
-        }
-        return false;
     }
 
     async createEmbed({ title, description, guild }: ICreateEmbed) {
